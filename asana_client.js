562e55219a65bc0506b2bd4a04d2835b78455d68
productiveme <jaco@productive.me>
2013-11-16 11:00:15 +0200

    Rollback

--- asana_client.js
+++ asana_client.js
@@ -27,5 +27,5 @@ Asana.requestCredential = function (options, credentialRequestCompleteCallback) 
         '&redirect_uri=' + Meteor.absoluteUrl('_oauth/asana?close') +
         '&state=' + credentialToken;
 
-  Accounts.oauth.initiateLogin(credentialToken, loginUrl, credentialRequestCompleteCallback);
+  Oauth.initiateLogin(credentialToken, loginUrl, credentialRequestCompleteCallback);
 };
