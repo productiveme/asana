4cb4494f3324a4bb21fe1357ea4412ebd314ba5f
productiveme <jaco@productive.me>
2013-11-16 10:59:49 +0200

    Rollback

--- asana_server.js
+++ asana_server.js
@@ -1,4 +1,32 @@
-Asana.whitelistedFields = ['name', 'email', 'workspaces'];
+Asana.whitelistedFields = ['name','email','workspaces'];
+
+Oauth.registerService('asana', 2, null, function(query) {
+
+  var response = getTokens(query);
+  var accessToken = response.accessToken;
+  var identity = response.identity;
+
+  var serviceData = {
+    accessToken: accessToken,
+    expiresAt: (+new Date) + (1000 * response.expiresIn),
+    id: response.identity.id
+  };
+
+  var fields = _.pick(identity, Asana.whitelistedFields);
+  _.extend(serviceData, fields);
+
+  // only set the token in serviceData if it's there. this ensures
+  // that we don't lose old ones (since we only get this on the first
+  // log in attempt)
+  if (response.refreshToken)
+    serviceData.refreshToken = response.refreshToken;
+
+  return {
+    serviceData: serviceData,
+    options: {profile: {name: identity.name}}
+  };
+
+});
 
 // returns an object containing:
 // - accessToken
@@ -6,23 +34,19 @@ Asana.whitelistedFields = ['name', 'email', 'workspaces'];
 // - refreshToken, if this is the first authorization request
 var getTokens = function (query) {
   var config = ServiceConfiguration.configurations.findOne({service: 'asana'});
-  if (!config) {
+  if (!config)
     throw new ServiceConfiguration.ConfigError("Service not configured");
-  }
 
   var response;
   try {
-    response = Meteor.http.post("https://app.asana.com/-/oauth_token",
-      {
-        params:
-          {
-            code: query.code,
-            client_id: config.clientId,
-            client_secret: config.secret,
-            redirect_uri: Meteor.absoluteUrl("_oauth/asana?close"),
-            grant_type: 'authorization_code'
-          }
-      });
+    response = Meteor.http.post(
+      "https://app.asana.com/-/oauth_token", {params: {
+        code: query.code,
+        client_id: config.clientId,
+        client_secret: config.secret,
+        redirect_uri: Meteor.absoluteUrl("_oauth/asana?close"),
+        grant_type: 'authorization_code'
+      }});
   } catch (err) {
     throw new Error("Failed to complete OAuth handshake with Asana. " + err.message);
   }
@@ -38,34 +62,4 @@ var getTokens = function (query) {
     };
   }
 };
-
-Accounts.oauth.registerService('asana', 2, null, function (query) {
-
-  var response = getTokens(query);
-  var accessToken = response.accessToken;
-  var identity = response.identity;
-
-  var serviceData = {
-    accessToken: accessToken,
-    expiresAt: (+new Date()) + (1000 * response.expiresIn),
-    id: response.identity.id
-  };
-
-  var fields = _.pick(identity, Asana.whitelistedFields);
-  _.extend(serviceData, fields);
-
-  // only set the token in serviceData if it's there. this ensures
-  // that we don't lose old ones (since we only get this on the first
-  // log in attempt)
-  if (response.refreshToken) {
-    serviceData.refreshToken = response.refreshToken;
-  }
-
-  return {
-    serviceData: serviceData,
-    options: {profile: {name: identity.name}}
-  };
-
-});
-
 
