d41da0413cf6d44c1e6b2420c3b88f91d48984c4
jacoswarts <jacos@privateproperty.co.za>
2016-07-08 14:59:45 +0200

    switch back to indexzero package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'productiveme:asana',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'Asana OAuth flow and client implementation of the Asana API',
   // URL to the Git repository containing the source code for this package.
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Npm.depends( {
-  "asana-api-oauth" : "0.2.1"
+  "asana-api" : "1.0.0"
 } );
 
 Package.onUse(function(api) {
